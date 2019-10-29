4832ca7d804753780900f3733c30ecdbed77b750
Jonathan Morales <johnathanmorales@Administrators-MacBook-Pro-67.local>
2019-10-29 10:09:07 -0400

    Walk through  each file and marshall it

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 )
 
+//var xi []map[string][]string
+
+// Scans the file and searches for imports
 func scanFile(f string) {
 	content, err := os.Open(f)
 	if err != nil {
@@ -21,14 +27,16 @@ func scanFile(f string) {
 	//rgx := regexp.MustCompile(`\((import(.*?)\)`)
 	importPresent := false
 
+	var imp []string
 	for scanner.Scan() {
 
 		if strings.Contains(scanner.Text(), ")") {
 			importPresent = false
 		}
 
-		if importPresent == true {
-			fmt.Println(scanner.Text())
+		if importPresent {
+			s := strings.TrimSpace(scanner.Text())
+			imp = append(imp, s)
 		}
 
 		if strings.Contains(scanner.Text(), "import") {
@@ -36,19 +44,49 @@ func scanFile(f string) {
 			s := strings.Split(scanner.Text(), "import")
 			str := strings.Join(s, "")
 			s = strings.Split(str, "(")
-
-			fmt.Println(strings.Join(s, ""))
+			str =  strings.Join(s, "")
+			str = strings.TrimSpace(str)
+			imp = append(imp, str)
 		}
-
-		//fmt.Println("text per line",scanner.Text())
-
 	}
-
+	fmt.Println(imp)
+	toJson(filepath.Base(f), imp)
 }
 
+func toJson(f string, imp [] string) {
+	 m := map[string][]string {
+	 	f: imp,
+	 }
+	 //xi = append(xi, m)
+	js, err := json.MarshalIndent(m, "", "\t")
+	j, err := json.MarshalIndent(m, "", "\t")
+	js = append(js, j...)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	 fmt.Println(string(js))
+}
+
+func listFiles(f string) {
+	err := filepath.Walk(f,
+		func(path string, info os.FileInfo, err error) error {
+			matched, err := regexp.MatchString(`.go$`, filepath.Base(path))
+			if matched {
+				scanFile(path)
+				//fmt.Println(path)
+				//fmt.Println(filepath.Base(path))
+			}
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+}
 func main() {
-
-	scanFile("./hi.go")
-	//fmt.Println(os.Args[1:])
+	 listFiles(os.Args[1])
+	//"../paperspace-project"
 }
---
+++ paperspace-project/hi.go
@@ -0,0 +1,28 @@
+package paperspace_project
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func main() {
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			matched, err := regexp.MatchString(`.go$`, filepath.Base(path))
+			if matched {
+				fmt.Println(filepath.Base(path))
+			}
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+
